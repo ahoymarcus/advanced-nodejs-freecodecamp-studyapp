a253de22a11462f876d9c929bed32a8e67ec2937
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-10-18 21:33:36 -0300

    Inserir definicoes do modulo bcrypt para criptografar nas rotas register e para LocalStrategy do modulo passport

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const ObjectID = require('mongodb').ObjectID;
+const bcrypt = require('bcrypt');
 
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
@@ -68,13 +69,17 @@ myDB(async client => {
 	
 	app.route('/register').post(
     (req, res, next) => {
-      myDataBase.findOne({ username: req.body.username }, function (err, user) {
+      const hash = bcrypt.hashSync(req.body.password, 12);
+			
+			myDataBase.findOne({ username: req.body.username }, function (err, user) {
         if (err) {
           next(err);
         } else if (user) {
           res.redirect('/');
         } else {
-          myDataBase.insertOne({ username: req.body.username, password: req.body.password }, (err, doc) => {
+          myDataBase.insertOne({ 
+						username: req.body.username, password: hash
+						}, (err, doc) => {
             if (err) {
               res.redirect('/');
             } else {
@@ -114,7 +119,12 @@ myDB(async client => {
 				console.log('User ' + username + ' attempted to log in.');
 				if(err) { return done(err); }
 				if (!user) { return done(null, false); }
-				if (password !== user.password) {return done(null, false); }
+				
+				// SUBSTITUIR por VALIDAÇÃO com HASH
+				/*if (password !== user.password) {return done(null, false); }*/
+				if (!bcrypt.compareSync(password, user.password)) {
+					return done(null, false);
+				}
 				
 				return done(null, user);
 			});
