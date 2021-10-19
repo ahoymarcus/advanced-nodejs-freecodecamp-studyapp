9d156b6d2b81ba2055fc60d99879b562131a829c
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-10-19 16:41:14 -0300

    Criar uma funcao para emitir notificacoes http e passar um contador com o numero de clientes http conectados.

--- public/client.js
+++ public/client.js
@@ -7,6 +7,10 @@ $(document).ready(function () {
 	// library on the page in chat.pug.
 	let socket = io();
 	
+	socket.on('user count', function(data) {
+		console.log(data);
+	});
+	
 	// Form submittion with new message in field with id 'm'
   $('form').submit(function () {
     var messageToSend = $('#m').val();
