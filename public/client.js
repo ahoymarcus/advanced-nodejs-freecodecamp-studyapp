b6f9a2ea088738271454b17fb568a87725edc43b
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-10-19 16:28:52 -0300

    Importar o modulo socket.io em server.js e criar um servidor HTTP. Passar o servidor http para o objeto io de socket.io. Criar um evento em io.on() e acrescentar uma variavel de socket.io em client.js

--- public/client.js
+++ public/client.js
@@ -1,5 +1,13 @@
+// This file's full path is /public/client.js
 $(document).ready(function () {
-  // Form submittion with new message in field with id 'm'
+  /* Global io */
+	// Freecodecamp: The comment suppresses 
+	// the error you would normally see since 'io' // is not defined in the file. We've already 
+	// added a reliable CDN to the Socket.IO
+	// library on the page in chat.pug.
+	let socket = io();
+	
+	// Form submittion with new message in field with id 'm'
   $('form').submit(function () {
     var messageToSend = $('#m').val();
 
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const auth = require('./auth.js');
 
 
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 
 fccTesting(app); //For FCC testing purposes
 
@@ -40,6 +43,10 @@ myDB(async client => {
 	routes(app, myDataBase);
 	auth(app, myDataBase);
 	
+	io.on('connection', socket => {
+		console.log('A user has connected');
+	});
+	
 }).catch(e => {
 	app.route('/').get((req, res) => {
 		res.render('pug', {
@@ -52,7 +59,7 @@ myDB(async client => {
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
 
