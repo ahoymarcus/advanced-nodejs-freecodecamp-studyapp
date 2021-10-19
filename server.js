6f8446aa96e948af03f8806d6109c614f6600001
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-10-18 21:14:11 -0300

    Criar mais 2 rotas: Logout e Register. Criar logica para inserir novo registro no Mongodb.

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ myDB(async client => {
 		res.render('pug', {
 			title: 'Connected to Database',
 			message: 'Please login',
-			showLogin: true
+			showLogin: true,
+			showRegistration: true
 		});
 	});
 	
@@ -57,6 +58,43 @@ myDB(async client => {
 		res.render(process.cwd() + '/views/pug/profile', {
 			username: req.user.username
 		});
+	});
+	
+	app.route('/logout').get((req, res) => {
+		req.logout();
+		
+		res.redirect('/');
+	});
+	
+	app.route('/register').post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, function (err, user) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne({ username: req.body.username, password: req.body.password }, (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              next(null, doc.ops[0]);
+            }
+          });
+        }
+      });
+    },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+	
+	
+	
+	
+	app.use((req, res, next) => {
+		res.status(404).type('text').send('Not Found');
 	});
 	
 	
